1e545ed94a7863c2bd514b851628758f075c2705
Saúl Orlando Alvarez Vargas <alvarezvar@outlook.com>
2017-03-30 14:40:47 -0600

    Ejemplo de imagenes 1

---
+++ Camara/Camara/Camara.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Camara.Droid.MainApplication, Camara.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5aca054d913356da3f8dbefd611c8e8d0.MainApplication.class, md5aca054d913356da3f8dbefd611c8e8d0.MainApplication.__md_methods);
+		
+	}
+}
---
+++ Camara/Camara/Camara/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Camara
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void TomarFotografia(object sender, EventArgs e)
+        {
+            var opciones_almacenamiento = new StoreCameraMediaOptions()
+            {
+                SaveToAlbum = true,
+                Name = "Foto_HRMG.jpg"
+            };
+
+            var foto = await CrossMedia.Current.TakePhotoAsync(opciones_almacenamiento);
+            Foto_HRMG.Source = ImageSource.FromStream(() =>
+            {
+                var stream = foto.GetStream();
+                foto.Dispose();
+                return stream;
+            });
+        }
+
+        private async void ElegirImagen(object sender, EventArgs e)
+        {
+            if (CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var imagen = await CrossMedia.Current.PickPhotoAsync();
+                if (imagen != null)
+                {
+                    Foto_HRMG.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = imagen.GetStream();
+                        imagen.Dispose();
+                        return stream;
+                    });
+                }
+            }
+        }
+	}
+}
